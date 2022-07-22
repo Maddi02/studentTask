281934ac23cd6a7dd0bc412ed506c3098e43248c
LeonMue <leon.m111@web.de>
2022-07-22 16:34:41 +0200

    rename TrafficLightChannel becuse of NameError

--- solution/with_oop_and_datastructures/RPi_led_controller.py
+++ solution/with_oop_and_datastructures/RPi_led_controller.py
@@ -6,7 +6,7 @@ from light_controller import LightController
 from traffic_light import TrafficLight
 from light_controller import LightController
 
-class __TrafficLightChannel(Enum):
+class TrafficLightChannel(Enum):
     RED = 7
     YELLOW = 5
     GREEN = 3
@@ -16,16 +16,16 @@ class RPiLedController(LightController):
         GPIO.setwarnings(False)
         GPIO.cleanup()
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(__TrafficLightChannel.RED.value, GPIO.OUT)
-        GPIO.setup(__TrafficLightChannel.YELLOW.value, GPIO.OUT)
-        GPIO.setup(__TrafficLightChannel.GREEN.value, GPIO.OUT)
+        GPIO.setup(TrafficLightChannel.RED.value, GPIO.OUT)
+        GPIO.setup(TrafficLightChannel.YELLOW.value, GPIO.OUT)
+        GPIO.setup(TrafficLightChannel.GREEN.value, GPIO.OUT)
     
     def apply(self, tl: TrafficLight):
-        self.__adaptLight(tl.red, __TrafficLightChannel.RED)
-        self.__adaptLight(tl.yellow, __TrafficLightChannel.YELLOW)
-        self.__adaptLight(tl.green, __TrafficLightChannel.GREEN)
+        self.__adaptLight(tl.red, TrafficLightChannel.RED)
+        self.__adaptLight(tl.yellow, TrafficLightChannel.YELLOW)
+        self.__adaptLight(tl.green, TrafficLightChannel.GREEN)
         
-    def __adaptLight(self, on: bool, channel: __TrafficLightChannel):
+    def __adaptLight(self, on: bool, channel: TrafficLightChannel):
         requiredSignal = GPIO.HIGH if on else GPIO.LOW
         
         if GPIO.input(channel.value) == requiredSignal:
